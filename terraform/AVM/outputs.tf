e7f6bdaf97fbe04791a6ff008559cce5d2055b67
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2023-04-15 08:53:12 -0500

    update vm

--- terraform/AVM/outputs.tf
+++ terraform/AVM/outputs.tf
@@ -2,6 +2,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+output "vm_nsg" {
+  value = azurerm_network_security_group.vm_nsg.name
+}
+
 output "vm_name" {
   value = azurerm_linux_virtual_machine.vm_vm.name
 }
--- terraform/AVM/post-create.sh
+++ terraform/AVM/post-create.sh
@@ -16,24 +16,24 @@ crontab /tmp/crontab.tmp
 rm /tmp/crontab.tmp
 
 #################################### Docker ########################################
-# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 
-# sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
-# sudo mkdir -m 0755 -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-# echo \
-#   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo apt-get update -y
+sudo apt-get update
 
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # https://docs.docker.com/engine/install/linux-postinstall/
-# sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER
 
-# sudo apt-get install -y docker-compose-plugin
+sudo apt-get install -y docker-compose-plugin
